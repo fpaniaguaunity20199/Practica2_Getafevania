b0c76547f1e03f56ba409c9808685d9321224120
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-11-08 14:04:25 +0100

    Escena de iluminacion 2D

---
+++ Assets/_GameAssets/Scripts/Player/PlayerDark.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDark : MonoBehaviour
+{
+    public float speed;
+    float x, y;
+    private Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+
+        rb2d.velocity = new Vector2(x * Time.deltaTime * speed, rb2d.velocity.y);
+    }
+}
